5eec2e9124580518429c23652fb09973e32b6184
Esteban Donis <estebandonis10@gmail.com>
2022-07-24 19:22:17 -0600

    cambios

--- Laboratorio3.kt
+++ Laboratorio3.kt
@@ -2,68 +2,33 @@
 data class ItemData(
     var originalPos: Int,
     var originalValue: Any,
-    var type: String? = null,
+	var type: String? = null,
     var info: String? = null
 )
 // -----------------------
 
 fun main() {
-    val result = processList(listOf(10, "Enero", null, true))
-    print(result)
-}
+        val result = processList(listOf(20, 25, 2, 7, "hola", "", true, false, null, 2.0))
+        println(result)
+    }
 
 fun processList(inputList: List<Any?>?): List<ItemData>? {
-    var OriginalPos: Int
     var contador: Int = 0
-
     var lista: MutableList<ItemData>? = mutableListOf()
-
+    if (inputList == null) return null
     inputList?.forEach{
-        OriginalPos = contador
-        contador += 1
         when (it) {
-            is String -> {lista?.add(ItemData(
-                originalPos = OriginalPos,
-                originalValue = it,
-                type = "cadena",
-                info = "L"+(it).length
-            ))}
-            is Int -> {lista?.add(ItemData(
-                originalPos = OriginalPos,
-                originalValue = it,
-                type = "entero",
-                info = typeInt(it)
-            ))}
-            is Boolean -> {lista?.add(ItemData(
-                originalPos = OriginalPos,
-                originalValue = it,
-                type = "booleano",
-                info = if ((it) == true) "Verdadero" else "Falso"
-            ))}
+            is String -> {lista?.add(ItemData(originalPos = contador, originalValue = it, type = "cadena", info = "l"+(it).length))}
+            is Int -> {lista?.add(ItemData(originalPos = contador, originalValue = it, type = "entero", info = typeInt(it)))}
+            is Boolean -> {lista?.add(ItemData(originalPos = contador, originalValue = it, type = "booleano",info = if ((it) == true) "verdadero" else "falso"))}
             null -> {}
-            else -> {lista?.add(ItemData(OriginalPos, it))}
+            else -> {lista?.add(ItemData(contador, it))}
         }
-    } ?: {
-        return null
+        contador += 1
     }
     return lista
-    
 }
-
-
 fun typeInt(item: Int): String?{
-    val Info: String = ""
-    if ((item % 10) == 0){
-        Info = "m10"
-    }
-    else if ((item % 5) == 0){
-        Info = "m5"
-    }
-    else if ((item % 2) == 0){
-        Info = "m2"
-    }
-    else {
-        return null
-    }
+    val Info: String = if ((item % 10) == 0) "m10" else if ((item % 5) == 0) "m5" else if ((item % 2) == 0) "m2" else return null
     return Info
 }
